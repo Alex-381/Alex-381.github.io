a658ef6840f5c39d7d4699fbcf7283071bb3f208
Alex-381 <aspadaccino@gmail.com>
2022-12-12 20:47:21 +0200

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,16 @@
+//Hook up the tweet display
+
+$(document).ready(function() {
+						   
+	name = '';
+	
+	$(".countdown").countdown({
+				date: "12 dec 2022 20:17:00",
+				format: "on"
+			},
+			
+			function() {
+				window.location.href = 'you-win.html';
+			});
+
+});	
